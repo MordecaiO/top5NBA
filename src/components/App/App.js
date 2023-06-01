5c1c26e82f57e806c170264cfdc4469d55ac6cb4
mordecaioladimeji <cai.oladimeji@gmail.com>
2023-06-01 07:10:35 +0100

    Update styling
    Adjust height and width of body, root, and App elements to fill webpage.
    Change positoning of MainRow component to the center of the webpage.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <MainRow>
+      <MainRow className="main-row">
         <PlayerCard />
       </MainRow>
     </div>
