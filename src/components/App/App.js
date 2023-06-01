000019665b66cae7316b68cf21af53faea2f67bf
mordecaioladimeji <cai.oladimeji@gmail.com>
2023-06-01 20:04:12 +0100

    Update Component
    Add TitleBox component to PlayerCard.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,12 @@ import PlayerCard from "../PlayerCard/PlayerCard";
 import { MainRow } from "../MainRow/MainRow";
 import "./App.css";
 
+const team = { primaryColor: "red" };
 function App() {
   return (
     <div className="App">
       <MainRow className="main-row">
-        <PlayerCard />
+        <PlayerCard team={team} />
         <PlayerCard />
         <PlayerCard />
         <PlayerCard />
--- src/components/PlayerCard/PlayerCard.js
+++ src/components/PlayerCard/PlayerCard.js
@@ -3,11 +3,10 @@ import styled from "styled-components";
 const StyledCard = styled.div`
   height: 379px;
   width: 300px;
-  
   border-radius: 10px;
   transition: background 0.8s;
   overflow: hidden;
-  background-color:rgb(209 26 28) ;
+  background-color: rgb(209 26 28);
   background-size: 300px
   box-shadow: 0 70px 63px -60px #000000;
   display: flex;
@@ -19,16 +18,30 @@ const StyledCard = styled.div`
 const InnerBorder = styled.div`
   height: 359px;
   width: 280px;
-
   border-width: 1px;
   border-radius: 0.5px;
   background-image: url(https://c8.alamy.com/zooms/9/ff9d1fe4132b41afbe2ee31f4739bf2a/2da09b5.jpg);
+  display: flex;
+  justify-content: center;
+`;
+
+const TitleBox = styled.div`
+  height: 30px;
+  width: 250px;
+  background-color: rgb(209 26 28);
+  border-radius: 25px;
+  border-width: 2.5px;
+  border-style: solid;
+  border-color: rgb(196 195 32);
+  border-top: none;
 `;
 
 const PlayerCard = () => {
   return (
     <StyledCard>
-      <InnerBorder></InnerBorder>
+      <InnerBorder>
+        <TitleBox></TitleBox>
+      </InnerBorder>
     </StyledCard>
   );
 };
