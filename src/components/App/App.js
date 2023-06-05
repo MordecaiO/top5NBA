dcfbe75c73508d0be353382e6e132a5fd574447f
mordecaioladimeji <cai.oladimeji@gmail.com>
2023-06-05 19:42:11 +0100

    Add Logo Color Variable
    Add Hex codes for the primary and secondary colours for each
    NBA team logo.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,19 @@ import { MainRow } from "../MainRow/MainRow";
 import "./App.css";
 
 const teamPrimaryColor = {
-  Atlanta: "red",
-  Boston: "green",
-  Warriors: "yellow",
-  Lakers: "purple",
-  Pheonix: "orange",
+  Atlanta: "#C8102E",
+  Boston: "#007A33",
+  Warriors: "#1D428A",
+  Lakers: "#552583",
+  Pheonix: "#1D1160",
+};
+
+const teamSecondaryColor = {
+  Atlanta: "##FDB927",
+  Boston: "##BA9653",
+  Warriors: "#FFC72C",
+  Lakers: "#FDB927",
+  Pheonix: "#E56020",
 };
 
 function App() {
